ceb1a4f33df4e12497ea6c5bfec301f76069e1f6
Amine BELLEBNA <mohamed_el_amine.bellebna@g.enp.edu.dz>
2024-06-19 15:10:27 +0000

    Add train script

--- train_script.sh
+++ train_script.sh
@@ -1,16 +1,20 @@
+
 # Train additional tasks on standard latent
 python train.py -mt compression --tasks Zoom FFX -norm -bs 15
+
 
 # Train multiple task without loss normalization
 python train.py -mt compression --tasks HiFiC Zoom FFX -norm -bs 15
 python train.py -mt compression --tasks HiFiC Zoom -norm -bs 15
 python train.py -mt compression --tasks HiFiC FFX -norm -bs 15
 
+
 # Train with loss normalization
 # python train.py -mt compression --tasks HiFiC Zoom FFX -norm -bs 15 --norm_loss
 # python train.py -mt compression --tasks HiFiC Zoom -norm -bs 15 --norm_loss
 # python train.py -mt compression --tasks HiFiC FFX -norm -bs 15 --norm_loss
 
-# Train with loss normalization using test task only
+
+# Train with loss normalization using test-task only
 # python train.py -mt compression --tasks HiFiC Zoom -norm -bs 15 --norm_loss --test_task
 # python train.py -mt compression --tasks HiFiC FFX -norm -bs 15 --norm_loss --test_task
